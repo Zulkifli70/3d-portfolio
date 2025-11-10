b5a176d50628f66e623805063cf6d3679db77a72
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-10 21:39:24 +0700

    add email.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -155,7 +155,7 @@ const techStackIcons = [
 const expCards = [
   {
     review:
-      "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+      "Zulk brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
     imgPath: "/images/jaya.png",
     logoPath: "/images/jayantara.png",
     title: "UI UX Designer (Internship)",
@@ -164,19 +164,6 @@ const expCards = [
       "Developed and maintained user-facing features for the Hostinger website.",
       "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
       "Optimized web applications for maximum speed and scalability.",
-    ],
-  },
-  {
-    review:
-      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: "/images/exp2.png",
-    logoPath: "/images/logo2.png",
-    title: "Full Stack Developer",
-    date: "June 2020 - December 2023",
-    responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-      "Contributed to open-source projects that were used with the Docker ecosystem.",
     ],
   },
 ];
